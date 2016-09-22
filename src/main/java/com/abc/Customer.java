be2258a17158005415e2442dae0f5a21ebc189eb
User <User@User-PC.home>
2016-09-22 15:18:44 -0700

    1.A customer can transfer between their accounts 2.Change Maxi-Savings accounts to have an interest rate of 5% assuming 3.no withdrawals in the past 10 days otherwise 0.1% Interest rates should accrue daily (incl. weekends)

--- src/main/java/com/abc/Customer.java
+++ src/main/java/com/abc/Customer.java
@@ -6,73 +6,93 @@ import java.util.List;
 import static java.lang.Math.abs;
 
 public class Customer {
-    private String name;
-    private List<Account> accounts;
+	private String name;
+	private List<Account> accounts;
 
-    public Customer(String name) {
-        this.name = name;
-        this.accounts = new ArrayList<Account>();
-    }
+	public Customer(String name) {
+		this.name = name;
+		this.accounts = new ArrayList<Account>();
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public Customer openAccount(Account account) {
-        accounts.add(account);
-        return this;
-    }
+	public Customer openAccount(Account account) {
+		accounts.add(account);
+		return this;
+	}
 
-    public int getNumberOfAccounts() {
-        return accounts.size();
-    }
+	public int getNumberOfAccounts() {
+		return accounts.size();
+	}
 
-    public double totalInterestEarned() {
-        double total = 0;
-        for (Account a : accounts)
-            total += a.interestEarned();
-        return total;
-    }
+	public double totalInterestEarned() {
+		double total = 0;
+		for (Account a : accounts)
+			total += a.interestEarned();
+		return total;
+	}
 
-    public String getStatement() {
-        String statement = null;
-        statement = "Statement for " + name + "\n";
-        double total = 0.0;
-        for (Account a : accounts) {
-            statement += "\n" + statementForAccount(a) + "\n";
-            total += a.sumTransactions();
-        }
-        statement += "\nTotal In All Accounts " + toDollars(total);
-        return statement;
-    }
+	public String dailyAccrual() {
+		for (Account a : accounts) {
+			a.deposit(a.dailyAddedInterest());
+		}
+		return "daily accrual made";
+	}
 
-    private String statementForAccount(Account a) {
-        String s = "";
+	public String getStatement() {
+		String statement = null;
+		statement = "Statement for " + name + "\n";
+		double total = 0.0;
+		for (Account a : accounts) {
+			statement += "\n" + statementForAccount(a) + "\n";
+			total += a.sumTransactions();
+		}
+		statement += "\nTotal In All Accounts " + toDollars(total);
+		return statement;
+	}
 
-       //Translate to pretty account type
-        switch(a.getAccountType()){
-            case Account.CHECKING:
-                s += "Checking Account\n";
-                break;
-            case Account.SAVINGS:
-                s += "Savings Account\n";
-                break;
-            case Account.MAXI_SAVINGS:
-                s += "Maxi Savings Account\n";
-                break;
-        }
+	private String statementForAccount(Account a) {
+		String s = "";
 
-        //Now total up all the transactions
-        double total = 0.0;
-        for (Transaction t : a.transactions) {
-            s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
-            total += t.amount;
-        }
-        s += "Total " + toDollars(total);
-        return s;
-    }
+		// Translate to pretty account type
+		switch (a.getAccountType()) {
+		case Account.CHECKING:
+			s += "Checking Account\n";
+			break;
+		case Account.SAVINGS:
+			s += "Savings Account\n";
+			break;
+		case Account.MAXI_SAVINGS:
+			s += "Maxi Savings Account\n";
+			break;
+		}
 
-    private String toDollars(double d){
-        return String.format("$%,.2f", abs(d));
-    }
+		// Now total up all the transactions
+		double total = 0.0;
+		for (Transaction t : a.transactions) {
+			s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " "
+					+ toDollars(t.amount) + "\n";
+			total += t.amount;
+		}
+		s += "Total " + toDollars(total);
+		return s;
+	}
+
+	private String toDollars(double d) {
+		return String.format("$%,.2f", abs(d));
+	}
+
+	public String transferMoney(double amount, Account a, Account b) {
+		double customersFunds = a.sumTransactions();
+		if (customersFunds >= amount) {
+			a.withdraw(amount);
+			b.deposit(amount);
+			return "transfer was successful";
+		}
+
+		return "you have only " + toDollars(customersFunds)
+				+ " , choose different amount";
+	}
 }
--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -4,15 +4,40 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateProvider {
-    private static DateProvider instance = null;
+	private static DateProvider instance = null;
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
+	public static DateProvider getInstance() {
+		if (instance == null)
+			synchronized (DateProvider.class) {
+				if (instance == null)
+					instance = new DateProvider();
+			}
+		return instance;
+	}
 
-    public Date now() {
-        return Calendar.getInstance().getTime();
-    }
+	// avoid serialization break
+	public Object readResolve() {
+		return getInstance();
+	}
+
+	public boolean getLeap() {
+		Date date = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+
+		int year = cal.get(Calendar.YEAR);
+		if (year % 400 == 0)
+			return true;
+		if ((year % 4 == 0) && (year % 100 != 0))
+			return true;
+		return false;
+	}
+
+	public int dateDiff(Date a, Date b) {
+		return (int) (a.getTime() - b.getTime()) / (1000 * 60 * 60 * 24);
+	}
+
+	public Date now() {
+		return Calendar.getInstance().getTime();
+	}
 }
